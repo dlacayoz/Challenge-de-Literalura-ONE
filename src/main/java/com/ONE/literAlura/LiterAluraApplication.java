402583cc75b2787bdae9a3d70e5cdc6e1883a625
dlacayoz <DANIEL.LACAYOZUNIGA@ucr.ac.cr>
2024-07-16 19:19:09 -0600

    Add files via upload

---
+++ src/main/java/com/ONE/literAlura/LiterAluraApplication.java
@@ -0,0 +1,28 @@
+package com.ONE.literAlura;
+
+import com.ONE.literAlura.principal.Principal;
+import com.ONE.literAlura.repository.AutorRepository;
+import com.ONE.literAlura.repository.LibroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterAluraApplication implements CommandLineRunner {
+
+	@Autowired
+	private LibroRepository libroRepository;
+	@Autowired
+	private AutorRepository autorRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterAluraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal(libroRepository, autorRepository);
+		principal.mostrarMenu();
+	}
+}
