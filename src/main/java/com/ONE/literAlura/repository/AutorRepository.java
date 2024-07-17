aae15fe5815fbf56f0420a6423aa4e0e63d32d3a
dlacayoz <DANIEL.LACAYOZUNIGA@ucr.ac.cr>
2024-07-16 19:15:59 -0600

    Update AutorRepository.java

--- src/main/java/com/ONE/literAlura/repository/AutorRepository.java
+++ src/main/java/com/ONE/literAlura/repository/AutorRepository.java
@@ -1,1 +1,13 @@
+package com.ONE.literAlura.repository;
 
+import com.ONE.literAlura.modelo.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+
+    @Query("SELECT a FROM Autor a WHERE a.fechaNacimiento <= :fechaDeMuerte AND a.fechaDeceso >= :fechaDeMuerte")
+    List<Autor> autoresPorFechaDeMuerte(int fechaDeMuerte);
+}
